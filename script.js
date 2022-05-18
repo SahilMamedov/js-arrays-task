e1bf1d0e956b5f2c25274040010f31aa1c39987e
SahilMamedov <sahilim@code.edu.az>
2022-05-19 02:15:58 +0400

    Feat word length

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+       // task 1
+          
 // let numbers=[4,7,9,43,12,65,34,23]
 
 // numbers.sort(function(a, b){return b-a});
@@ -5,7 +7,7 @@
 // console.log(numbers);
 
 
-
+       // task 2
 
 // let mont=["Yanvar","Fevral","Mart","Aprel","May","Iyun","Iyul","Avqust","Sentyabr"]
 // let num=1;
@@ -17,3 +19,15 @@
 //   });
 //   console.log(newMont)
 
+
+//         task 3
+
+// function wordlength(word){
+
+//     let arrayOfStrings = word.split(" ")
+
+//   let wordlengthArr= arrayOfStrings.map((value)=>value.length)
+
+// return wordlengthArr
+// }
+// console.log(wordlength("Salam olsun hamiya")) 
