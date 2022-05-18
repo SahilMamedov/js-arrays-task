ca740aa3ca8d37d423185ec7c28c884a62539575
SahilMamedov <sahilim@code.edu.az>
2022-05-19 01:07:30 +0400

    Feat: Mont task

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-let numbers=[4,7,9,43,12,65,34,23]
+// let numbers=[4,7,9,43,12,65,34,23]
 
-numbers.sort(function(a, b){return b-a});
+// numbers.sort(function(a, b){return b-a});
 
-console.log(numbers);
+// console.log(numbers);
+
+
+
+let mont=["Yanvar","Fevral","Mart","Aprel","May","Iyun","Iyul","Avqust","Sentyabr"]
+
+function montnum(mont){
+
+    let newMont=[];
+    let num=1;
+for (let i = 0; i < mont.length; i++) {
+    
+    mont[i]+= -num
+    newMont.push( mont[i])
+    num++;
+    
+
+    
+}
+return newMont
+}
+console.log(montnum(mont))
